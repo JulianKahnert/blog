f39b15c96e4352aa742c94831db28761d43d90ab
Julian Kahnert <mail@juliankahnert.de>
2021-08-09 10:36:54 +0200

    test readingTime plugin

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", .branch("master")),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin.git", from: "0.1.0"),
         .package(url: "https://github.com/SwiftyGuerrero/CNAMEPublishPlugin", from: "0.1.0"),
-        .package(url: "https://github.com/artrmz/ReadTimePublishPlugin", from: "0.1.1")
+        .package(url: "https://github.com/alexito4/ReadingTimePublishPlugin", from: "0.2.0")
     ],
     targets: [
         .target(
