14834c2471c830839d87b514eea9da047de98c9f
Julian Kahnert <mail@juliankahnert.de>
2021-08-09 10:57:30 +0200

    fix III

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "PersonalWebsite",
-            dependencies: ["Publish", "SplashPublishPlugin", "CNAMEPublishPlugin", "ReadTimePublishPlugin"]
+            dependencies: ["Publish", "SplashPublishPlugin", "CNAMEPublishPlugin", "ReadingTimePublishPlugin"]
         )
     ]
 )
